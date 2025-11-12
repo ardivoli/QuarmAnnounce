682ec08e0f0097e2b660e3fdd04318fa9cd64b98
Ardivoli <173434132+ardivoli@users.noreply.github.com>
2025-11-12 15:56:29 -0600

    fix download speakers script

--- scripts/download-speakers.sh
+++ scripts/download-speakers.sh
@@ -1,4 +1,6 @@
-!#/bin/bash
+!#/usr/bin/bash
+
+mkdir -p ./speakers
 
 curl -L -o speakers/en_US-amy-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx.json
 curl -L -o speakers/en_US-amy-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx
