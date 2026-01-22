7a2cf1a81f75963271f3c8834814efae70097f3a
Kai Yao <173434132+ardivoli@users.noreply.github.com>
2026-01-21 21:30:14 -0600

    fixed release script using wrong speaker path

--- scripts/download-speakers.sh
+++ scripts/download-speakers.sh
@@ -1,6 +1,6 @@
 !#/usr/bin/bash
 
-mkdir -p ./speakers
+mkdir -p ./resources/speakers
 
-curl -L -o speakers/en_US-amy-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx.json
-curl -L -o speakers/en_US-amy-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx
+curl -L -o ./resources/speakers/en_US-amy-medium.onnx.json https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx.json
+curl -L -o ./resources/speakers/en_US-amy-medium.onnx https://huggingface.co/rhasspy/piper-voices/resolve/main/en/en_US/amy/medium/en_US-amy-medium.onnx
